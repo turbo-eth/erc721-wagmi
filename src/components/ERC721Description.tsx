ce46e5f5301136ccfc6bd0a65c975d545b3e09a6
kames <kamescg@gmail.com>
2023-01-15 15:27:40 -0300

    fix: dependencies

--- src/components/ERC721Description.tsx
+++ src/components/ERC721Description.tsx
@@ -4,8 +4,28 @@ import { useERC721Metadata } from '../hooks/useERC721Metadata';
 
 interface ERC721DescriptionProps {
   className?: string;
-  address: string;
   tokenId: string;
+  address: string;
+  abi?: any;
+  functionName?: string;
+  chainId?: number;
+  args?: any[];
+  cacheOnBlock?: boolean;
+  watch?: boolean;
+  cacheTime?: number;
+  enabled?: boolean;
+  isDataEqual?: (a: any, b: any) => boolean;
+  structuralSharing?: (a: any, b: any) => boolean;
+  scopeKey?: string;
+  staleTime?: number;
+  suspense?: boolean;
+  select?: () => void;
+  overrides?: {
+    [key: string]: any;
+  };
+  onSuccess?: () => void;
+  onError?: () => void;
+  onSettled?: () => void;
 }
 
 export const ERC721Description = ({
--- src/components/ERC721OwnerOf.tsx
+++ src/components/ERC721OwnerOf.tsx
@@ -4,9 +4,30 @@ import classNames from 'classnames';
 
 import useERC721Read from '../hooks/useERC721Read';
 
-interface ERC721OwnerOfProps extends ContractReadOptions {
+interface ERC721OwnerOfProps {
   className?: string;
   tokenId: string;
+  address: string;
+  abi?: any;
+  functionName?: string;
+  chainId?: number;
+  args?: any[];
+  cacheOnBlock?: boolean;
+  watch?: boolean;
+  cacheTime?: number;
+  enabled?: boolean;
+  isDataEqual?: (a: any, b: any) => boolean;
+  structuralSharing?: (a: any, b: any) => boolean;
+  scopeKey?: string;
+  staleTime?: number;
+  suspense?: boolean;
+  select?: () => void;
+  overrides?: {
+    [key: string]: any;
+  };
+  onSuccess?: () => void;
+  onError?: () => void;
+  onSettled?: () => void;
 }
 
 export const ERC721OwnerOf = ({
